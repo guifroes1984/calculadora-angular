e5caa15c7aaa7159fc34aea7c1a6a2f31ba41223
guifroes1984 <guilhermefroes26@gmail.com>
2025-04-02 23:27:39 -0300

    Adicionando ação no componente.

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class CalculadoraComponent {
 
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  calcularResultado() {
+    console.log("Chamando");
+    this.resultado = this.numero1 + this.numero2;
+  }
+
 }
